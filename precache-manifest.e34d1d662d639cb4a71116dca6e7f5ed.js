5e2a39c4c188f35a9f6c8c3e36d1c68b49882371
cryptoseabook <cryptoseabook@gmail.com>
2018-11-29 19:08:39 +1100

    Updates

---
+++ precache-manifest.e34d1d662d639cb4a71116dca6e7f5ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "03e000d67d19de50e4c8",
+    "url": "/perfekt-code-test/static/js/runtime~main.03e000d6.js"
+  },
+  {
+    "revision": "98f4f061cd33321b6424",
+    "url": "/perfekt-code-test/static/js/main.98f4f061.chunk.js"
+  },
+  {
+    "revision": "ae7b6f5285044c3a9b4d",
+    "url": "/perfekt-code-test/static/js/1.ae7b6f52.chunk.js"
+  },
+  {
+    "revision": "98f4f061cd33321b6424",
+    "url": "/perfekt-code-test/static/css/main.ee94ba63.chunk.css"
+  },
+  {
+    "revision": "597539ca89589a62d206d298a3d21b34",
+    "url": "/perfekt-code-test/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c75689f6ef33d6cdff61e064f30cd91.js"
+  "/perfekt-code-test/precache-manifest.e34d1d662d639cb4a71116dca6e7f5ed.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/perfekt-code-test/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
