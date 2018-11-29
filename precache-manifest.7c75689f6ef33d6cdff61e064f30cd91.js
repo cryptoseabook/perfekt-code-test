cb7236e1d40ca8b245cfb4445624b39e7a27f493
cryptoseabook <cryptoseabook@gmail.com>
2018-11-29 19:02:21 +1100

    Updates

---
+++ precache-manifest.7c75689f6ef33d6cdff61e064f30cd91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "98f4f061cd33321b6424",
+    "url": "/static/js/main.98f4f061.chunk.js"
+  },
+  {
+    "revision": "ae7b6f5285044c3a9b4d",
+    "url": "/static/js/1.ae7b6f52.chunk.js"
+  },
+  {
+    "revision": "98f4f061cd33321b6424",
+    "url": "/static/css/main.ee94ba63.chunk.css"
+  },
+  {
+    "revision": "6c913ab3785cb6ed42eb3105695a2896",
+    "url": "/index.html"
+  }
+];
